6c91d5944c90f9e1987304b8fca626055c7e4814
MarceloCarvalhoLopes <marcelolcarvalho@gmail.com>
2022-10-26 06:07:04 -0300

    deleting data

--- index.js
+++ index.js
@@ -71,6 +71,24 @@ app.post("/game",(req,res) => {
 
 })
 
+app.delete("/game/:id",(req, res) => {
+    if(isNaN(req.params.id)){
+        res.sendStatus(400);
+    }else{
+        var id = parseInt(req.params.id);
+                                 //achar um game que tenha um id igual ao parametro
+        var index = DB.games.findIndex(g => g.id == id);
+
+        if (index == -1) {
+            res.sendStatus(404);
+
+        }else{
+            DB.games.splice(index,1);
+            res.sendStatus(200);
+        }
+    }    
+});
+
 app.listen(45678,()=>{
     console.log("API IS RUNNING!")
 })
